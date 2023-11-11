da7c5668164f63e00d4a7b50d11f94c1858f4012
Pyae Bhone Zaw <pyaebhonezaw2003@gmail.com>
2023-11-12 01:35:16 +0630

    footer ui

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Book\BookController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
+// Route::get('/', [BookController::class, 'index']);
+
+Route::get('/books', [BookController::class, 'index']);
+
+Route::get('/books/add-book', [BookController::class, 'add']);
+
+Route::post('books/add-book', [BookController::class, 'create']);
+
+Route::get('/books/detail/{id}', [BookController::class, 'detail']);
+
+
+
+
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');;
