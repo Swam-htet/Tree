c8fc012aecaace1b981b3ca489d80c56a9c3680b
Alphar <swamhtet129@gmail.com>
2023-11-12 09:21:50 +0630

    UI changes

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,20 +1,44 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
+    <div class="container-md-fluid container">
 
-@if (session('info'))
-<div class="alert alert-info">
-    {{ session('info') }}
-</div>
-@endif
+        {{--        alert box message for session information       --}}
+        @if (session('info'))
+            <div class="alert alert-info">
+                {{ session('info') }}
+            </div>
+        @endif
 
-<h1>Books List</h1>
+        <h1>Books List</h1>
 
-<ul>
- @foreach($books as $book)
- <li>{{ $book['title'] }}</li>
- @endforeach
- </ul>
+        <div class="row flex-wrap">
+            @foreach($books as $book)
+                <div class="col-12 col-md-6 col-xl-3 p-3">
+                    <div class="card p-3">
+                        <div class="card-title">{{ $book['title'] }}</div>
+                        <div class="card-body">
+                            <div class="row flex-wrap">
+                                <div class="col-6 col-md-12">
+                                    <img src="https://images.dog.ceo/breeds/weimaraner/n02092339_363.jpg"
+                                         class="card-img">
+                                </div>
+                                <div class="col-6 col-md-12 mt-lg-2">
+                                    <p>
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                                        unknown printer took.
+                                    </p>
+                                </div>
+                            </div>
 
- </div>
+                        </div>
+                        <div class="card-footer">
+                            <a class="btn btn-outline-primary" href="https://www.google.com">Detail</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,35 +4,36 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Book\BookController;
 use App\Http\Controllers\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// home route -> to render home page
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// about -> to render about page
+Route::get('/about', [HomeController::class, 'about']);
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-// Route::get('/', [BookController::class, 'index']);
+// content route -> to render content page
+Route::get('/content', [HomeController::class, 'content']);
 
+
+
+// book route -> to render books/index page
 Route::get('/books', [BookController::class, 'index']);
 
+// book route -> to render books/add-book page
 Route::get('/books/add-book', [BookController::class, 'add']);
-
 Route::post('books/add-book', [BookController::class, 'create']);
 
+// book route -> to render books/detail page
 Route::get('/books/detail/{id}', [BookController::class, 'detail']);
+
+// search box -> to render books/search page
+Route::get('/books/search?keyword={searchParam}', [HomeController::class, 'index']);
+
+// search book route -> to render books/search page
+Route::get('/books/az-list/{searchParam}', [HomeController::class, 'index']);
 
 
 
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
